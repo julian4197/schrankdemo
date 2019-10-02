42893fe6c43c8335105cccf74841394fd8f8cb4c
HansHuepp <51884778+HansHuepp@users.noreply.github.com>
2019-10-02 16:00:03 +0200

    Add files via upload

---
+++ htdocs/action_page.php
@@ -0,0 +1,29 @@
+
+
+
+<?php 
+$name1 = "Hans";
+$psw1 = "H";
+if ($_POST["uname"] == $name1 and $_POST["psw"] == $psw1) 
+{
+    header("Location: index.html");
+
+  } 
+  else {
+    echo "Zugriff verweigert!";
+  }
+  
+  
+  
+  ?>
+
+
+
+
+
+ 
+
+
+
+
+
